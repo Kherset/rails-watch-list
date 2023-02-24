403ff130c5534c6aaf1456fb60bccfbe3e4d1210
jeremy twardowski <kherset@protonmail.com>
2023-02-24 16:57:01 +0100

    Mise en ligne fin jour 5 rails

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -13,6 +13,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "lists#index"
 
-  resources :lists, only: [:show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :delete]
+  resources :lists, only: [:show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create, :destroy]
   end
 end
